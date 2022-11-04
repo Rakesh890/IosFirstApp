a97904880fa5e92eedc9450aa1af4f7000a08f5e
FirstiOSApp <rakeshprajapat78122@gmail.com>
2022-11-05 00:26:01 +0530

    Initial Commit

---
+++ Shared/IosFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosFirstAppApp.swift
+//  Shared
+//
+//  Created by Rakesh on 05/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IosFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

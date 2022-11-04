b68b865c45167e8570ce9fa8f4a7a53c18c086f8
FirstiOSApp <rakeshprajapat78122@gmail.com>
2022-11-05 00:33:50 +0530

    My First text part done

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ///name
+            Text("Rakesh Prajapat").font(.title).fontWeight(.medium).foregroundColor(Color.purple).padding(.leading, 30.0).padding(.vertical,50.0).padding(.trailing,50.0).accessibilityIdentifier("rakesh_text")
+            
+            ///buttons add on
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Text("item")
+            };
+            
+        }
     }
 }
 
